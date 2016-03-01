5e65217fbdac021ec65693b21d04608a5bb8285e
ElusiveSpirit <2447785@gmail.com>
2016-03-01 23:41:50 +0300

    Первый коммит для алгоритмов.

---
+++ ALG/a1.c
@@ -0,0 +1,66 @@
+//
+//  main.c
+//  a_1_2
+//
+//  Created by Konstantin on 27.02.16.
+//  Copyright (c) 2016 Manyakhin. All rights reserved.
+//
+
+/*    TASK
+********************************************************
+    Вывести разложение натурального числа n на простые множители.
+  Простые множители должны быть упорядочены
+  по возрастанию и разделены пробелами.
+
+   2 <= n <= 10 ^ 6
+********************************************************
+*/
+
+#include "stdlib.h"
+#include "stdio.h"
+#include "errno.h"
+
+// Возвращает массив простых множителей
+int getMultiplierArray(int number, int *resultArray) {
+    size_t size = 0;
+    if (resultArray == NULL)
+        return 0;
+
+    // Поиск простых множителей до 1
+    while (number > 1) {
+        // При нахождении первого множителя число number
+        // делится на найденное число
+        for (int i = 2; i <= number; i++) {
+            if (number % i == 0) {
+                resultArray[size] = i;
+
+                resultArray = (int* )realloc(resultArray, (++size + 1) * sizeof(int));
+                if (errno == ENOMEM || resultArray == NULL)
+                    return 0;
+
+                number /= i;
+                break;
+            }
+        }
+    }
+    return size;
+}
+
+int main() {
+    int number = 0;
+    int size = 0;
+    int *array = (int* )malloc(sizeof(int));;
+
+    if (scanf("%d", &number) != 1)
+        return 0;
+
+    size = getMultiplierArray(number, array);
+    if (array == NULL)
+        return 0;
+
+    for (int i = 0; i < size; i++)
+        printf("%d ", array[i]);
+
+    free(array);
+    return 0;
+}
