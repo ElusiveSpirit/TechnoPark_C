73c6a0e539e1f09b0340162a58a035c163f921d1
ElusiveSpirit <2447785@gmail.com>
2016-03-04 09:27:54 +0300

    Фэил на 3 тесте.

--- ALG/main.c
+++ ALG/main.c
@@ -12,6 +12,51 @@
     на интервале [0, m] значения массива строго
     возрастают, а на интервале [m, n­1] строго убывают.
     Найти m за O(log m).
-    n ≤ 10000. 
+    n ≤ 10000.
 ********************************************************
 */
+
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "errno.h"
+
+
+int findMax(size_t size, int a[size]) {
+    if (size == 0) return 0;
+
+    int left = 0,
+        right = size - 1,
+        middle = size / 2;
+
+    while (a[middle] == a[right] && a[middle] == a[left]) {
+        if (a[middle] > a[left] && a[middle] > a[right]) {
+            left = (middle - left) / 2;
+            right = (middle - right) / 2;
+        } else if (a[middle] <= right) {
+            left = middle;
+            middle = right - left / 2;
+        } else {
+            right = middle;
+            middle = right - left / 2;
+        }
+
+    }
+    return a[middle];
+}
+
+int main() {
+    // Ввод массивов
+    int size = 0;
+    if (scanf("%d\n", &size) == 0)
+        return 0;
+    int a[size];
+    for (size_t i = 0; i < size; i++) {
+        if (scanf("%d", &a[i]) == 0)
+            return 0;
+    }
+
+    printf("%d\n", findMax(size, a));
+
+    return 0;
+}
